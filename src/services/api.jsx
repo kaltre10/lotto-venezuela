bc4b614953f4e56d7d309e4899a392302f12b153
Jason Hernandez <kaltre10@gmail.com>
2022-07-25 01:23:09 -0500

    error cors

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,8 +1,11 @@
 const api = (url, data, method) => {
     return fetch(url, {
         method,
+        mode: "cors",
         headers: {
-            'Content-Type': "application/json"
+            'Content-Type': "application/json",
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept"
         },
         body: JSON.stringify(data)
     });
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lotto-venezuela.netlify.app/'
+  base: 'https://pollamillonaria.com/'
 })
