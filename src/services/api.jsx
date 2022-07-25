ac13ae57ee5cb395c10cffbb193a8bc1167c8a2b
Jason Hernandez <kaltre10@gmail.com>
2022-07-25 01:30:38 -0500

    url .env

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,8 @@
 const api = (url, data, method) => {
     return fetch(url, {
         method,
-        mode: "cors",
         headers: {
             'Content-Type': "application/json",
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept"
         },
         body: JSON.stringify(data)
     });
